ccbea94193da565014b119d45858103a0aa3643e
s-davies <swdavies.contact@gmail.com>
2020-05-23 13:26:43 -0700

    refactor grid

--- src/grid.js
+++ src/grid.js
@@ -1,34 +1,9 @@
 export default class Grid {
   constructor() {
-    let that = this;
-    // draggables snap to droppables
-    $(".reward-1").draggable({
-      revert: "invalid",
-      snap: ".drop1",
-      snapMode: "inner",
-      snapTolerance: 30
-    });
-    $(".reward-2").draggable({
-      revert: "invalid",
-      snap: ".drop2",
-      snapMode: "inner",
-      snapTolerance: 30
-    });
-    $(".reward-3").draggable({
-      revert: "invalid",
-      snap: ".drop3",
-      snapMode: "inner"
-    });
-    $(".reward-4").draggable({
-      revert: "invalid",
-      snap: ".drop4",
-      snapMode: "inner"
-    });
-    $(".reward-5").draggable({
-      revert: "invalid",
-      snap: ".drop5",
-      snapMode: "inner"
-    });
+    
+    //bind removeReward function
+    this.removeReward = this.removeReward.bind(this);
+
     // change cursor on click and unclick of draggable
     $(".rewards-col div").on('mousedown', (e) => {
       $(".rewards-col div").css("cursor", "grabbing");
@@ -36,90 +11,34 @@ export default class Grid {
     $(".rewards-col div").on('mouseup', (e) => {
       $(".rewards-col div").css("cursor", "grab");
     });
-    //create droppables that draggables will stick to
-    $(".drop1").droppable({
-      accept: ".reward-1",
-      drop: function (event, ui) {
-        //re-enable the droppable that was previously occupied 
-        if (ui.draggable[0].dataset.parent !== "r1-drop0") {
-          $(`#${ui.draggable[0].dataset.parent}`).droppable("option", "disabled", false);
-        }
-        // disable new droppable
-        if (event.target.id !== "r1-drop0") {
-          $(`#${event.target.id}`).droppable("option", "disabled", true);
-        }
-        //reset the draggable parent to the new droppable
-        ui.draggable[0].dataset.parent = event.target.id;
-      }
-    });
-    $(".drop2").droppable({
-      accept: ".reward-2",
-      drop: function (event, ui) {
-        //re-enable the droppable that was previously occupied 
-        if (ui.draggable[0].dataset.parent !== "r2-drop0") {
-          $(`#${ui.draggable[0].dataset.parent}`).droppable("option", "disabled", false);
-        }
-        // disable new droppable
-        if (event.target.id !== "r2-drop0") {
-          $(`#${event.target.id}`).droppable("option", "disabled", true);
-        }
-        //reset the draggable parent to the new droppable
-        ui.draggable[0].dataset.parent = event.target.id;
-      }
-    });
-    $(".drop3").droppable({
-      accept: ".reward-3",
-      drop: function (event, ui) {
-        //re-enable the droppable that was previously occupied 
-        if (ui.draggable[0].dataset.parent !== "r3-drop0") {
-          $(`#${ui.draggable[0].dataset.parent}`).droppable("option", "disabled", false);
-        }
-        // disable new droppable
-        if (event.target.id !== "r3-drop0") {
-          $(`#${event.target.id}`).droppable("option", "disabled", true);
-        }
-        //reset the draggable parent to the new droppable
-        ui.draggable[0].dataset.parent = event.target.id;
-      }
-    });
-    $(".drop4").droppable({
-      accept: ".reward-4",
-      drop: function (event, ui) {
-        //re-enable the droppable that was previously occupied 
-        if (ui.draggable[0].dataset.parent !== "r4-drop0") {
-          $(`#${ui.draggable[0].dataset.parent}`).droppable("option", "disabled", false);
-        }
-        // disable new droppable
-        if (event.target.id !== "r4-drop0") {
-          $(`#${event.target.id}`).droppable("option", "disabled", true);
-        }
-        //reset the draggable parent to the new droppable
-        ui.draggable[0].dataset.parent = event.target.id;
-      }
-    });
-    $(".drop5").droppable({
-      accept: ".reward-5",
-      drop: function (event, ui) {
-        //re-enable the droppable that was previously occupied 
-        if (ui.draggable[0].dataset.parent !== "r5-drop0") {
-          $(`#${ui.draggable[0].dataset.parent}`).droppable("option", "disabled", false);
-        }
-        // disable new droppable
-        if (event.target.id !== "r5-drop0") {
-          $(`#${event.target.id}`).droppable("option", "disabled", true);
-        }
-        //reset the draggable parent to the new droppable
-        ui.draggable[0].dataset.parent = event.target.id;
-      }
-    });
-
-    this.removeReward = this.removeReward.bind(this);
-    $(".drop1 b").click({ param1: '1'}, this.removeReward);
-    $(".drop2 b").click({ param1: '2'}, this.removeReward);
-    $(".drop3 b").click({ param1: '3'}, this.removeReward);
-    $(".drop4 b").click({ param1: '4'}, this.removeReward);
-    $(".drop5 b").click({ param1: '5'}, this.removeReward);
     
+    for (let i = 1; i < 6; i++) {
+      // draggables snap to droppables
+      $(`.reward-${i}`).draggable({
+        revert: "invalid",
+        snap: `.drop${i}`,
+        snapMode: "inner",
+        snapTolerance: 30
+      });
+      //create droppables that draggables will stick to
+      $(`.drop${i}`).droppable({
+        accept: `.reward-${i}`,
+        drop: function (event, ui) {
+          //re-enable the droppable that was previously occupied 
+          if (ui.draggable[0].dataset.parent !== `r${i}-drop0`) {
+            $(`#${ui.draggable[0].dataset.parent}`).droppable("option", "disabled", false);
+          }
+          // disable new droppable
+          if (event.target.id !== `r${i}-drop0`) {
+            $(`#${event.target.id}`).droppable("option", "disabled", true);
+          }
+          //reset the draggable parent to the new droppable
+          ui.draggable[0].dataset.parent = event.target.id;
+        }
+      });
+      //add click handler to upper right x to remove reward
+      $(`.drop${i} b`).click({ param1: `${i}` }, this.removeReward);
+    }
   }
   //////////////////////////////////////////////////////////////////////////////
   removeReward (event) {
