ff453d817a91e98c279d220c63b3d8fd3ffc5d28
s-davies <swdavies.contact@gmail.com>
2020-05-23 13:39:24 -0700

    fix removal bug

--- src/grid.js
+++ src/grid.js
@@ -42,7 +42,10 @@ export default class Grid {
   }
   //////////////////////////////////////////////////////////////////////////////
   removeReward (event) {
+    //get the draggable element to be removed
     const el = event.target.parentElement;
+    //set the correct information for the new draggable that will be created
+    //and placed back at the beginning of the row
     let eventPic;
     let eventText;
     switch (event.data.param1) {
@@ -69,18 +72,22 @@ export default class Grid {
       default:
         break;
     }
-
+    //re-add element to starting position of row
     $(el.parentElement).append(
       `<div class='reward-${event.data.param1}'><b>X</b><i class='${eventPic}'></i><h3>${eventText}</h3></div>`
     );
-    $(`.drop${event.data.param1} b`).click({ param1: '1' }, this.removeReward);
+    //add click handler for new element's x
+    $(`.drop${event.data.param1} b`).click({ param1: `${event.data.param1}` }, this.removeReward);
+    //make new element draggable
     $(`.reward-${event.data.param1}`).draggable({
       revert: "invalid",
       snap: `.drop${event.data.param1}`,
       snapMode: "inner",
       snapTolerance: 30
     });
+    //reenable the previously disabled droppable element
     $(`#${el.dataset.parent}`).droppable("option", "disabled", false);
+    //remove the element whose x was clicked
     $(el).remove();
   }
 
